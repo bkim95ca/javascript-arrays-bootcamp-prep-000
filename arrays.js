93a65d0fc36ac68ef9b0ee0bb7fdb982789ac1c9
Brandon Kim <bkim95ca@gmail.com>
2018-02-27 22:48:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var addElementToBeginnigofArray = [1,2,3] {
   addElementToBeginnigofArray([1],"foo")
   return addElementToBeginnigofArray
