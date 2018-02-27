3488fb42d018aebe9b362209d87e7e3ee837ce9a
Brandon Kim <bkim95ca@gmail.com>
2018-02-27 22:44:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-var addElementToBeginningOfArray = [1]
 addElementToBeginnigofArray([1], "foo")
 var destructivelyAddElementToBeginningOfArray = chocolateBars.unshift("hersheys")
