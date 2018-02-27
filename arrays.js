932c4e023cab88859b42229dc1aa6889bf0edfb9
Brandon Kim <bkim95ca@gmail.com>
2018-02-27 22:59:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray([1],'foo') {
   return ['foo', ...1]
 }
+
+function destructivelyAddElementToBeginningOfArray([1],'foo')
